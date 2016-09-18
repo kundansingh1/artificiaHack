2e9c0b3e69a68ec8dbadca4410ff3a8461cc456c
Kundan Singh <kundan.singh979@gmail.com>
2016-09-18 13:43:29 +0530

    Init

---
+++ constants.js
@@ -0,0 +1,5 @@
+const api_key = "xxxx";
+
+module.exports = {
+    api_key: api_key
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+var client = require("artifacia");
+var constants = require('./constants.js');
+
+var api_key = constants.api_key;
+client.artifacia(api_key);
+
+// var sample_data = require("./data.json");
+// client.upload_item_data(sample_data,function(result){console.log(result)});
+
+
+var sample_prod_id = 2;
+var num = 4;
+var filters = { 'category': 1, 'pattern': 1 };
+client.get_visual_recommendation(sample_prod_id, num, filters, function (result) { console.log(result) });
