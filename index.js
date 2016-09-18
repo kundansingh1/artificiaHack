14e37cabbb26b47ec347e5e16c35d46fc0521e9a
Kundan Singh <kundan.singh979@gmail.com>
2016-09-18 20:04:34 +0530

    Integerated Artificia API

--- index.js
+++ index.js
@@ -3,15 +3,67 @@
 var client = require("artifacia");
 var constants = require('./constants.js');
 
+
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
 var api_key = constants.api_key;
 client.artifacia(api_key);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 4040;        // set our port
+
+
+var router = express.Router();
+
+router.get('/:prod_id', function (req, res) {
+
+    console.log(req.params.prod_id);
+
+    var sample_prod_id = req.params.prod_id;
+    var num = 5;
+    var filters = { 'category': 1, 'pattern': 1 };
+
+    var result = client.get_visual_recommendation(sample_prod_id, num, filters, function (result) { 
+        res.json(result);
+    });
+
+
+});
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/api', router);
+
+// Add headers
+
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
 // var sample_data = require("./data.json");
 // client.upload_item_data(sample_data,function(result){console.log(result)});
-
-
-var sample_prod_id = 2;
-var num = 4;
-var filters = { 'category': 1, 'pattern': 1 };
-
-client.get_visual_recommendation(sample_prod_id, num, filters, function (result) { console.log(1) });
--- public/Extretion_files/script.js
+++ public/Extretion_files/script.js
@@ -42,13 +42,13 @@ function searchServer(obj) {
         success: function (data) {
             currentResult = data;
 
-            console.log(currentResult);
+            // console.log(currentResult);
 
             $("#broadResult").show();
             $("#top-destination-wrapper .gap-20").html("");
             $("#soloResultContainer").html("");
 
-            console.log(data);
+            // console.log(data);
 
             for (var i = 0; i < data.length; i++) {
                 var prod_id = data[i].id;
@@ -78,28 +78,30 @@ function searchServer(obj) {
             }
 
             $('.place').click(function () {
-                
+
                 var prod_id = $(this).attr('prod_id');
-                
+                $("#apiResultContainer").show();
+
+
                 currentResult.map(function (obj) {
                     if (obj.id === prod_id) {
                         var appendData = '<div class="hotel-item-list">\
-                                            <div class="image" style="background-image:url('+obj.url+');"></div>\
+                                            <div class="image" style="background-image:url('+ obj.url + ');"></div>\
                                             <div class="content">\
                                                 <div class="heading">\
-                                                    <h4>'+obj.prod_name+'</h4>\
-                                                    <p><i class="fa fa-map-marker text-primary"></i> '+ obj.location+'</p>\
+                                                    <h4>'+ obj.prod_name + '</h4>\
+                                                    <p><i class="fa fa-map-marker text-primary"></i> '+ obj.location + '</p>\
                                                 </div>\
                                                 <div class="short-info">\
                                                     Use securing confined his shutters. Delightful as he it acceptance an solicitude discretion reasonably. Carriage we husbands advanced an perceive greatest expense on demesne ye he.\
                                                 </div>\
                                             </div>\
                                             <div class="absolute-bottom">\
-                                                <p class="text-primary"><i class="fa fa-check-circle"></i> '+obj.category+' <span class="mh-10">|</span> <i class="fa fa-check-circle"></i> '+obj.pattern+'</p>\
+                                                <p class="text-primary"><i class="fa fa-check-circle"></i> '+ obj.category + ' <span class="mh-10">|</span> <i class="fa fa-check-circle"></i> ' + obj.pattern + '</p>\
                                             </div>\
                                             <div class="absolute-right">\
                                                 <div class="price-wrapper">\
-                                                    <p class="price"><span class="block">start from</span><span class="number">$'+obj.price+'</span> <span class="block">avg / night</span></p>\
+                                                    <p class="price"><span class="block">start from</span><span class="number">$'+ obj.price + '</span> <span class="block">avg / night</span></p>\
                                                     <a href="#" class="btn btn-danger btn-sm">Details</a>\
                                                 </div>\
                                             </div>\
@@ -108,37 +110,85 @@ function searchServer(obj) {
                         $("#soloResultContainer").show();
 
                         $("#soloResultContainer").html(appendData);
+                        $("#apiResults .gap-20").html("");
+
+
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://localhost:4040/api/' + prod_id,
+                            async: false,
+                            success: function (data) {
+                                var requestData = data.prod_ids.toString();
+
+                                $.ajax({
+                                    type: 'GET',
+                                    url: 'http://aditechsolutions.com/nitesh/artifacia/GetRecomendations.php?ids=' + requestData,
+                                    async: false,
+                                    success: function (data) {
+                                        console.log(data);
+
+                                        for (var i = 0; i < data.length; i++) {
+                                            var prod_id = data[i].id;
+                                            var prod_name = data[i].prod_name;
+                                            var price = data[i].price;
+                                            var url = data[i].url;
+                                            var location = data[i].location;
+
+                                            var appendData = '<div class="col-xss-12 col-xs-6 col-sm-4 col-md-3 place" prod_id="' + prod_id + '">\
+                                        <div class="top-destination-item">\
+                                            <a>\
+                                                <div class="image">\
+                                                    <img src="'+ url + '" alt="Top Destinations">\
+                                                </div>\
+                                                <div class="content">\
+                                                    <div class="row gap-10">\
+                                                        <div class="col-xs-7 place">\
+                                                            <h4>' + prod_name + '</h4>\
+                                                            <p>'+ location + '</p>\
+                                                        </div>\
+                                                    </div>\
+                                                </div>\
+                                            </a>\
+                                        </div>\
+                                    </div>';
+                                            $("#apiResults .gap-20").append(appendData);
+                                        }
+                                    }
+                                });
+
+                            }
+                        });
+
+
+                        // alert();
+
+                        // var num = 4;
+                        // var filters = { 'category': 1, 'pattern': 1 };
+
+                        // var baseUrl = 'https://api.artifacia.com/v1/recommendation/similar/' + prod_id + '/' + num + '?';
+
+                        // for (var key in filters) {
+                        //     baseUrl = baseUrl + key + '=' + filters[key] + '&'
+                        // }
+
+                        // console.log(baseUrl);
+
+                        // $.ajax({
+                        //     type: 'GET',
+                        //     beforeSend: function (request) {
+                        //         request.setRequestHeader("Authority", authorizationToken);
+                        //     },
+                        //     url: baseUrl,
+                        //     async: false,
+                        //     success: function (data) {
+                        //         console.log(data);
+                        //     }
+                        // });
 
                         return true;
                     }
                 });
             })
-
         }
     });
 }
-/*
-
-				<div class="hotel-item-list">
-					<div class="image" style="background-image:url('http://crenoveative.com/envato/extretion/images/top-destinations/02.jpg');"></div>
-					<div class="content">
-						<div class="heading">
-							<h4>Hotel Metropolitan Tokyo</h4>
-							<p><i class="fa fa-map-marker text-primary"></i> Tokyo, Japan</p>
-						</div>
-						<div class="short-info">
-							Use securing confined his shutters. Delightful as he it acceptance an solicitude discretion reasonably. Carriage we husbands advanced an perceive greatest expense on demesne ye he.
-						</div>
-					</div>
-					<div class="absolute-bottom">
-						<p class="text-primary"><i class="fa fa-check-circle"></i> Breakfast Included <span class="mh-10">|</span> <i class="fa fa-check-circle"></i> Free Wifi in Room</p>
-					</div>
-					<div class="absolute-right">
-						<div class="price-wrapper">
-							<p class="price"><span class="block">start from</span><span class="number">$187</span> <span class="block">avg / night</span></p>
-							<a href="#" class="btn btn-danger btn-sm">Details</a>
-						</div>
-					</div>
-				</div>
-
-                */
